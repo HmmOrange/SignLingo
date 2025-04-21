8e7a48b604ddad121f208f874540cdf742f00cbf
HmmOrange <leanhduy0206@gmail.com>
2025-04-21 09:16:52 +0700

    Change to light theme

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => window.open('./login', '_blank')}> 
+          Login
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
